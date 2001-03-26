c6acd4c7af5d1e622b0e0be3bbafff44a8a8a522
Tomas Abrahamsson <tab@lysator.liu.se>
2001-03-26 15:39:27 +0000

    Documented the public interface of ErlMBox and ErlNode. Changed Nodeup/down-subscriptions to not include the node name.

--- erl_node.py
+++ erl_node.py
@@ -23,7 +23,25 @@ _pidCount = 1
 _serial = 0
 
 class ErlMBox:
+    """This class provides an mbox, which is equivalent to an
+    erlang-process. It is intenteded to be used in a single-threaded
+    application. You must provide a callback routine for incoming
+    messages."""
+
     def __init__(self, node, pid, msgCallback):
+        """The recommended way to create mboxes is through the method
+        CreateMBox in the ErlNode class.
+
+        NODE            = <instance of ErlNode>
+                        The node to which this mbox belongs.
+        PID             = <instance of ErlPid>
+                        The pid for this mbox
+        MSG-CALLBACK    = <function(MSG): void>
+                        A callback to call for incoming messages
+                        to this mbox. The callback should take one
+                        argument: the message. Its return value is
+                        ignored.
+        """
         self._node = node
         if msgCallback == None:
             msgCallback = self._Sink
@@ -33,24 +51,101 @@ class ErlMBox:
         self._nodeDownSubscriptions = {}
 
     def Self(self):
+        """Return the pid for this mbox.
+
+        Returns: <instance of ErlPid>
+        Throws:  nothing
+        """
         return self._pid
 
     def Send(self, dest, msg):
+        """Send, to DEST, the message MSG.
+
+        DEST    = <instance of ErlPid> | string | <instance of ErlAtom> |
+                  tuple(NODE, PROC-NAME)
+                  NODE = PROC-NAME = string | <instance of ErlAtom>
+                The destination to which the message is to be sent.
+                This can is either a pid for an mbox (on the same node or on a
+                different node), or a registered name (as string or atom) on
+                the same node, or a tuple specifying a node and a registered
+                name on that node.
+
+        MSG     = <term>
+                The message to be sent.
+
+        Returns: void
+        Throws:  <<to-be-documented>>
+
+        Send a message. There is no result indicating whether the delivery
+        of the message was successful or not. If the DEST is located on
+        another node, a connection to that node is automatically set up
+        and maintained, unless already connected.
+
+        This method returns immediately. Any necessarry queuing,
+        possibly due to network traffic congestion, is handled
+        automatically by the node.
+        """
         self._node.SendMsgFromMBox(self, dest, msg)
 
     def RegisterName(self, name):
+        """Register a NAME for this mbox.
+
+        NAME    = string | <instance of ErlAtom>
+                The name to register this mbox as.
+
+        Returns: void
+        Throws:  <<to-be-documented>>
+
+        Only on name can be registered for each mbox.
+        """
         self._node.RegisterNameForMBox(self, name)
 
     def UnregisterName(self):
+        """Unregister the name that was registered for this mbox.
+        Returns: void
+        Throws:  <<to-be-documented>>
+        """
         self._node.UnregisterNameForMBox(self)
 
     def Link(self, otherEnd):
+        """Link this mbox to another pid/mbox.
+        Currently, this is not implemented."""
         pass
 
     def Unlink(self, otherEnd):
+        """Unlink another pid/mbox.
+        Currently, this is not implemented."""
         pass
 
     def SendRPC(self, remoteNode, mod, fun, args, cb):
+        """Send an rpc to REMOTE-NODE, MOD, FUN, ARGS. Call CB for the answer.
+
+        REMOTE-NODE     = string | <instance of ErlAtom>
+                        The node to send the call to
+        MOD             =  string | <instance of ErlAtom>
+                        The module
+        FUN             = string | <instance of ErlAtom>
+                        The name of the function to call
+        ARGS            = list(<term>)
+                        The argument list
+        CB              = <function(RESULT): void>
+                          REMOTE-NODE = string
+                          RESULT = <term>
+                        A callback function to be called when the answer
+                        to the rpc callback receives. The callback is called
+                        with one arg: the result. Its return value is ignored.
+                        If the remote node goes down during execution, the
+                        callback is called with this value:
+                          tuple(<ErlAtom("EXIT")>,
+                                tuple(<ErlAtom("nodedown")>,
+                                      <ErlAtom(<REMOTE-NODE-NAME>)>))
+
+        Returns: void
+        Throws:  <<to-be-documented>>
+
+        Send an request to execute a function in a module on a remote node.
+        As for the Send method, this method returns immediately.
+        """
         if type(mod) == types.StringType:
             mod = erl_term.ErlAtom(mod)
         if type(fun) == types.StringType:
@@ -69,7 +164,7 @@ class ErlMBox:
         # Register a nodedown-callback for this node, so
         # we can call any rpc callbacks in case the node goes down
         if not self._nodeDownSubscriptions.has_key(remoteNodeName):
-            id = self._node.NodeDownSubscribe(remoteNodeName, self._NodeDown)
+            id = self._node.NodeDownSubscribe(self._NodeDown)
             self._nodeDownSubscriptions[remoteNodeName] = id
 
         # Now send the rpc-message
@@ -82,10 +177,20 @@ class ErlMBox:
     ## Routines to be called from the node only
     ##
     def Msg(self, sourceNodeName, msg):
+        """This routine is intended to be called only from mbox's node.
+
+        SOURCE-NODE-NAME        = string
+        MSG                     = <term>
+
+        Returns: void
+        Throws:  nothing
+
+        An incoming message
+        """
         if type(msg) == types.TupleType and \
            len(msg) == 2 and \
            erl_term.IsErlAtom(msg[0]) and \
-           msg[0].atomText == "rex"and \
+           msg[0].atomText == "rex" and \
            len(self._pendingRPCs) > 0:
             self._RPCAnswer(sourceNodeName, msg[1])
         else:
@@ -112,10 +217,17 @@ class ErlMBox:
                      erl_term.ErlAtom(nodeName))))
 
     def _Sink(self, *a, **kw):
+        """Message sink."""
         pass
 
 
 class ErlNode:
+    """This class implements a node, which is equivaluent to an erlang node.
+    It is intended to be used in a single-threaded applucation.
+
+    MBoxes, which corresponds to erlang processes, can be created for
+    communication.
+    """
     # Early operations 
     CTRLMSGOP_LINK = 1
     CTRLMSGOP_SEND = 2
@@ -137,6 +249,26 @@ class ErlNode:
     # end of operations
     
     def __init__(self, nodeName, opts=erl_opts.ErlNodeOpts()):
+        """Constructor.
+
+        NODE-NAME       = string
+                        The name for this node.
+
+        OPTS            = <instance of ErlNodeOpts>
+
+        Creates an ErlNode. The name of the node is determined by NODE-NAME
+        as described below. A node-name consists of two parts: an alive-name
+        and a host-name, separated by an `@'. The host-name can be short
+        (not including the domain) or long (including the domain). Short and
+        long node-names must not be mixed among the nodes in a system, see
+        the erlang documentation for further details.
+          1. If the NODE-NAME contains an `@', then NODE-NAME is used
+             unchanged as name for the node.
+          2. If the NODE-NAME does not contain an `@', then the node's name
+             is constructed as NODE-NAME + "@" + host-name, where
+             host-name is either on short or long form, depending on what is
+             specified in the OPTS.
+        """
         shortNodeNames = opts.GetShortNodeNames()
         self._nodeName = erl_common.AlignNodeName(nodeName, shortNodeNames)
         self._opts = opts
@@ -153,8 +285,8 @@ class ErlNode:
         self._registeredNames = {}      # mapping name    --> pid
         self._registeredPids = {}       # mapping pid     --> name
 
-        self._nodeUpCb = {}
-        self._nodeDownCb = {}           # mapping nodeName --> [(id,callback)]
+        self._nodeUpCb = []             # stores (id, callback)
+        self._nodeDownCb = []           # stores (id, callback)
         self._cbId = 0
 
         self._server = erl_node_conn.ErlNodeServerSocket(self._nodeName,
@@ -166,6 +298,18 @@ class ErlNode:
 
         
     def CreateMBox(self, msgCallback=None):
+        """Creates an mbox, which is equivalent to an erlang process.
+
+        MSG-CALLBACK    = <function(MSG): void>
+                        A callback function to call for incoming messages.
+
+        Returns: <instance of ErlMBox>
+        Throws:  nothing
+
+        This creates an mbox, which is equivalent to an erlang process.
+        Messages to the mbox are delivered as callbacks to the callback
+        function.
+        """
         mboxPid = self._CreatePid()
         mbox = ErlMBox(self, mboxPid, msgCallback)
         self._pids[mboxPid] = mbox
@@ -173,6 +317,23 @@ class ErlNode:
         return mbox
 
     def Ping(self, remoteNodeName, pingCallback):
+        """Ping a remote node.
+
+        REMOTE-NODE-NAME        = string | <instance of ErlAtom>
+                                The node to ping
+        PING-CALLBACK           = <function(RESULT): void>
+                                A callback to call for deliverance of the
+                                ping result. RESULT = "pong" | "pang".
+
+        Returns: void
+        Throws:  <<to-be-documented>>
+
+        Try to ping a remote node. A connection to that node is established
+        unless already connected. Whether or not the remote node is up or
+        down is indicated as by the argument to the callback function:
+          "pong": the remote node is alive and there is a connection to it
+          "pang": the remote node is down.
+        """
         if not "@" in remoteNodeName:
             raise "Bad node name for remote node"
         if self._ongoingPings.has_key(remoteNodeName):
@@ -186,49 +347,105 @@ class ErlNode:
             e.PortPlease2Req(nodeName, cb)
 
     def Publish(self):
+        """Publish this node to the EPMD, the Erlang Portmapper Daemon.
+        Returns: void
+        Throws:  nothing.
+
+        Publishing must be done for mboxes/processes on other nodes
+        to be able to establish contact with mboxes on this node.
+        The node is published automatically when it tries to contact
+        another node, for example due to a message to send or an rpc call.
+        """
         if not self._isServerPublished:
             self._epmd.Connect(self._EpmdConnectedOk, self._EpmdConnectFailed)
 
     def Unpublish(self):
+        """Removes the publication of this node to the EPMD.
+        Returns: void
+        Throws:  nothing
+        """
         if self._isServerPublished:
             self._epmd.Close()
             self._isServerPublished = 0
 
-    def NodeUpSubscribe(self, nodeName, cb):
+    def NodeUpSubscribe(self, cb):
+        """Subscribe to nodeup-information
+
+        CB        = <function("nodeup", NODE-NAME): void>
+
+        Returns:  ID = <id>
+        Throws:   nothing
+
+        Subscribe to nodeup-information, that is, to connections to
+        new nodes. The new connections may initiated by, for example,
+        a messages begin sent from this node to another one (a new
+        connection is automatically set up), or by another node
+        connection to this.
+
+        The ID returned can be used to unsubscribe the callback from further
+        nodeup-informations.
+        """
         id = self._cbId
         self._cbId = self._cbId + 1
-        if self._nodeUpCb.has_key(nodeName):
-            self._nodeUpCb[nodeName].append((id, cb))
-        else:
-            self._nodeUpCb[nodeName] = [(id, cb)]
+        self._nodeUpCb.append((id, cb))
         return id
 
-    def NodeUpUnsubscribe(self, nodeName, id):
-        if self._nodeUpCb.has_key(nodeName):
-            cbs = self._nodeUpCb[nodeName]
-            for (index, (cbid, cb)) in map(None, range(len(cbs)), cbs):
-                if id == cbid:
-                    del self._nodeUpCb[nodeName][index]
-                    return
+    def NodeUpUnsubscribe(self, id):
+        """Unsubscribe to nodeup-information
 
-    def NodeDownSubscribe(self, nodeName, cb):
+        ID              = <id>
+
+        Returns: void
+        Throws:  nothing
+
+        Unsubscribes to nodeup-information.
+        """
+        cbs = self._nodeUpCb
+        for (index, (cbid, cb)) in map(None, range(len(cbs)), cbs):
+            if id == cbid:
+                del self._nodeUpCb[index]
+                return
+
+    def NodeDownSubscribe(self, cb):
+        """Subscribe to nodedown-information for a certain node
+
+        CB        = <function("nodedown", NODE-NAME): void>
+
+        Returns:  ID = <id>
+        Throws:   nothing
+
+        Subscribe to nodedown-information, that is, to broken connections to
+        other nodes.
+
+        The ID returned can be used to unsubscribe the callback from further
+        nodedown-informations.
+        """
         id = self._cbId
         self._cbId = self._cbId + 1
-        if self._nodeDownCb.has_key(nodeName):
-            self._nodeDownCb[nodeName].append((id, cb))
-        else:
-            self._nodeDownCb[nodeName] = [(id, cb)]
+        self._nodeDownCb.append((id, cb))
         return id
 
-    def NodeDownUnsubscribe(self, nodeName, id):
-        if self._nodeDownCb.has_key(nodeName):
-            cbs = self._nodeDownCb[nodeName]
-            for (index, (cbid, cb)) in map(None, range(len(cbs)), cbs):
-                if id == cbid:
-                    del self._nodeDownCb[nodeName][index]
-                    return
+    def NodeDownUnsubscribe(self, id):
+        """Unsubscribe to nodedown-information
+
+        ID              = <id>
+
+        Returns: void
+        Throws:  nothing
+
+        Unsubscribes to nodedown-information.
+        """
+        cbs = self._nodeDownCb
+        for (index, (cbid, cb)) in map(None, range(len(cbs)), cbs):
+            if id == cbid:
+                del self._nodeDownCb[index]
+                return
 
     def DumpConnections(self):
+        """Dump connections. This method is intended for debugging purposes.
+        Returns: void
+        Throws:  nothing
+        """
         print "Connections:"
         for k in self._connections.keys():
             print "  %s --> %s" % (`k`, `self._connections[k]`)
@@ -352,18 +569,16 @@ class ErlNode:
         erl_common.Debug(M, "NODEUP: nodeName=%s connection=%s" % \
                          (nodeName, connection))
         self._connections[nodeName] = connection
-        if self._nodeUpCb.has_key(nodeName):
-            for (id, cb) in self._nodeUpCb[nodeName]:
-                cb("nodeup", nodeName)
+        for (id, cb) in self._nodeUpCb:
+            cb("nodeup", nodeName)
 
     def _NodeDown(self, connection, nodeName):
         erl_common.Debug(M, "NODENOWN: nodeName=%s connection=%s" % \
                          (nodeName, connection))
         if self._connections.has_key(nodeName):
             del self._connections[nodeName]
-            if self._nodeDownCb.has_key(nodeName):
-                for (id, cb) in self._nodeDownCb[nodeName]:
-                    cb("nodedown", nodeName)
+            for (id, cb) in self._nodeDownCb:
+                cb("nodedown", nodeName)
     
     def _PingEpmdResponse(self, result, portNum, nodeType, proto,
                           distVSNRange, nodeNameNoHost, extra,
