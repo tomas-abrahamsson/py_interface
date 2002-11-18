66640a3081f22e89269d59a6118319b3f48e87ac
Tomas Abrahamsson <tab@lysator.liu.se>
2002-11-18 22:37:31 +0000

    Two patches from Jimmy Olgeni <olgeni@uli.it> included.

--- erl_term.py
+++ erl_term.py
@@ -282,7 +282,7 @@ def _UnpackOneTerm(data):
         if dataLen < 5:
             return (None, data)
         n = _ReadInt4(data[1:5])
-        return (ErlNumber(i), data[5:])
+        return (ErlNumber(n), data[5:])
 
     elif data0 == MAGIC_FLOAT:
         if dataLen < 32:
@@ -552,11 +552,11 @@ def _PackOneTerm(term):
     
 def _PackString(term):
     if len(term) == 0:
-        return PackList([])
+        return _PackList([])
     elif len(term) <= 65535:
         return _PackInt1(MAGIC_STRING) + _PackInt2(len(term)) + term
     else:
-        return PackList(map(lambda c: ord(c), term))
+        return _PackList(map(lambda c: ord(c), term))
 
 def _PackList(term):
     if len(term) == 0:
