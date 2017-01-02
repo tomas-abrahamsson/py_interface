273e14e36ce69cc427b9f3f0e07064b02ee185e2
Tomas Abrahamsson <tab@lysator.liu.se>
2017-01-03 00:22:05 +0100

    Bugfix Emacs tab setting for Makefiles

--- .dir-locals.el
+++ .dir-locals.el
@@ -7,6 +7,10 @@
 	 (tab-width . 8)
 	 ;; Lines no longer than this
 	 (fill-column . 80)))
+
+ ;; However, for Makefiles, tabs are important
+ (makefile-mode . ((indent-tabs-mode . t)))
+
  ;; For Erlang files
  (erlang-mode . (
 		 ;; Use this spaces of indentation per level
